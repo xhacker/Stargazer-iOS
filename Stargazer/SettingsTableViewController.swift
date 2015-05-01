87b0f18509a7bdac99fc21c34ed96b2d9d29d53a
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2015-05-01 00:31:48 -0700

    Setup static cells for Settings, dynamic cells for Stars.

---
+++ Stargazer/SettingsTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsTableViewController.swift
+//  Stargazer
+//
+//  Created by Dongyuan Liu on 2015-04-30.
+//  Copyright (c) 2015 Ela. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
