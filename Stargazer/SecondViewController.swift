ddb14d3c10f655a4f52a71c38897c2d84d81b208
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2015-04-30 23:35:45 -0700

    Initial Commit

---
+++ Stargazer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Stargazer
+//
+//  Created by Dongyuan Liu on 2015-04-30.
+//  Copyright (c) 2015 Ela. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
