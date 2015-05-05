a0800784a19cc2e5d9edb428f6f66c8c666ee16b
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2015-05-04 23:35:38 -0700

    Popup sign in scene if not signed in

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '8.0'
+use_frameworks!
+
+pod 'KeychainAccess', '~> 1.2.0'
---
+++ Stargazer/TabBarViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TabBarViewController.swift
+//  Stargazer
+//
+//  Created by Dongyuan Liu on 2015-05-04.
+//  Copyright (c) 2015 Ela. All rights reserved.
+//
+
+import UIKit
+import KeychainAccess
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let keychain = Keychain(service: "build.ela.Stargazer")
+        if keychain.get("github-token") == nil {
+            performSegueWithIdentifier("signIn", sender: self)
+        }
+    }
+
+}
