f9a4b1c34641fe1cfd27f202c3aa8ce58029a07d
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2015-05-05 22:05:47 -0700

    Open GitHub auth page

---
+++ Stargazer/Config.sample.swift
@@ -0,0 +1,10 @@
+//
+//  Config.swift
+//  Stargazer
+//
+//  Created by Dongyuan Liu on 2015-05-05.
+//  Copyright (c) 2015 Ela. All rights reserved.
+//
+
+let kGitHubOAuthCliendID = "enter GitHub client ID here..."
+let kGitHubOAuthClientSecret = "enter GitHub client secret here..."
---
+++ Stargazer/SignInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignInViewController.swift
+//  Stargazer
+//
+//  Created by Dongyuan Liu on 2015-05-05.
+//  Copyright (c) 2015 Ela. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func signInPressed(sender: AnyObject) {
+        let URLString = "https://github.com/login/oauth/authorize?client_id=\(kGitHubOAuthCliendID)"
+        UIApplication.sharedApplication().openURL(NSURL(string: URLString)!)
+    }
+
+}
