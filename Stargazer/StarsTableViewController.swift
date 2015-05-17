6dddbc3d2075b93ce4a9c4f659ac50bbd81cf20a
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2015-05-17 11:29:40 -0700

    Populate Tags section in StarsTableViewController

--- Stargazer/StarsTableViewController.swift
+++ Stargazer/StarsTableViewController.swift
@@ -7,17 +7,38 @@
 //
 
 import UIKit
+import CoreData
 
 class StarsTableViewController: UITableViewController {
+    
+    enum Section: Int {
+        case Stars = 0
+        case Tags
+        static let count = 2
+        
+        var title: String? {
+            switch self {
+            case .Stars:
+                return "Stars"
+            case .Tags:
+                return "Tags"
+            }
+        }
+    }
+    
+    let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    var tags: [Tag] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let fetchRequest = NSFetchRequest(entityName: "Tag")
+        if let fetchResults = managedObjectContext!.executeFetchRequest(fetchRequest, error: nil) as? [Tag] {
+            tags = fetchResults
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,31 +48,36 @@ class StarsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    /*
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
+        return Section.count
     }
-    */
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Section(rawValue: section)?.title
+    }
 
-    /*
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
+        switch Section(rawValue: section)! {
+        case .Stars:
+            return 2
+        case .Tags:
+            return tags.count
+        }
     }
-    */
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
 
-        // Configure the cell...
+        switch Section(rawValue: indexPath.section)! {
+        case .Stars:
+            cell.textLabel?.text = "All"
+        case .Tags:
+            cell.textLabel?.text = tags[indexPath.row].name
+            cell.detailTextLabel?.text = String(tags[indexPath.row].repos.count)
+        }
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -70,21 +96,6 @@ class StarsTableViewController: UITableViewController {
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
     }
     */
 
